d6737cfb36fb934aa37751b24e3ed79289447680
Bishy <sonarbishal1@gmail.com>
2023-01-11 01:43:44 +0530

    second  commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-const express = require
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AllienDBex'
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', ()=>
+{
+    console.log('connected....')
+
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routers/aliens')
+app.use('/aliens',alienRouter)
+
+app.listen(9000,()=>{
+    console.log('Server Started')
+})
---
+++ routers/aliens.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const  router = express.Router()
+
+const Alien = require('../models/alien')
+
+router.get('/' , async(req,res)=>{
+    // router code here
+    // res.send('Get Request')
+    // console.log('Get request')
+
+    try{
+
+        const aliens = await Alien.find()
+        res.json(aliens)
+    }
+    catch(err)
+    {
+        res.send('Error'+ err)
+    }
+})
+
+
+router.post('/', async(req , res)=>{
+    
+const alien = new Alien(
+    {
+        name : req.body.name,
+        tech : req.body.tech,
+        sub : req.body.sub
+    })
+    try{
+  
+  const a1 = await alien.save()
+  res.json(a1)
+    }
+    catch(err)
+    {
+        res.send('Error')
+    }
+}
+)
+
+
+
+module.exports  = router
